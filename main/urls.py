9ffb766b87c022b1e126409eefc488bed907b524
Qurbonov2004 <dasturchi0204@gmail.com>
2024-01-31 21:57:05 +0500

    Yangi o'zgarishlar

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import main,login
+from .views import *
 urlpatterns=[
     path('',main, name='main'),
-    path('main/register/', login , name='register'),
+    path('main/register/', login , name='account'),
+    path('dashboard/',adminka, name='dashboard'),
+    path('category/list',category_list,name='category_list'),
+    path('category/create/',category_create,name='category_create'),
+    path('category/update/<int:id>/',category_update,name='category_update'),
+    path('category/delete/<int:id>/',category_delete,name='category_delete'),
+    path('product/list',product,name='product'),
+    path('product/create',product_create,name='product_create'),
+    path('product/delete/<int:id>/',product_delete,name='product_delete'),
+    path('product/update/<int:id>/',product_update,name='product_update'),
 ]
--- main/views.py
+++ main/views.py
@@ -24,7 +24,7 @@ def main(request):
       'wishList':wishList
    }
    
-   return render(request, 'index.html', context)
+   return render(request, 'asosiy/index.html', context)
 
 
 from django.shortcuts import render
@@ -32,44 +32,44 @@ from .models import Card, CardProduct, Category, Product, ProductImage, ProductR
 
 def cards(request):
     cards = Card.objects.all()
-    return render(request, 'index.html', {'cards': cards})
+    return render(request, 'asosiy/index.html', {'cards': cards})
 
 
 def card_product(request):
     card_products = CardProduct.objects.all()
-    return render(request, 'index.html', {'card_products': card_products})
+    return render(request, 'asosiy/index.html', {'card_products': card_products})
 
 
 def categories(request):
     categories = Category.objects.all()
-    return render(request, 'index.html', {'categories': categories})
+    return render(request, 'asosiy/index.html', {'categories': categories})
 
 
 def products(request):
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    return render(request, 'asosiy/index.html', {'products': products})
 
 
 def product_image(request):
     product_images = ProductImage.objects.all()
-    return render(request, 'index.html', {'product_images': product_images})
+    return render(request, 'asosiy/index.html', {'product_images': product_images})
 
 
 def product_review(request):
     product_reviews = ProductReview.objects.all()
-    return render(request, 'index.html', {'product_reviews': product_reviews})
+    return render(request, 'asosiy/.html', {'product_reviews': product_reviews})
 
 
 def wishlist(request):
     wishlists = WishList.objects.all()
-    return render(request, 'index.html', {'wishlists': wishlists})
+    return render(request, 'asosiy/.html', {'wishlists': wishlists})
 
 
 
 def login(request):
     if request.method == 'POST':
 
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
 
         try:
@@ -77,14 +77,14 @@ def login(request):
             user = User.objects.create_user(username=username, password=password)
 
 
-            return redirect('index.html')
+            return redirect('asosiy/index.html')
 
         except Exception as e:
             
-            return render(request, 'login.html', {'error_message': str(e)})
+            return render(request, 'asosiy/account.html', {'error_message': str(e)})
 
    
-    return render(request, 'login.html')
+    return render(request, 'asosiy/account.html')
 
 
 
@@ -92,4 +92,122 @@ def login(request):
 
 
 
+#dashboard
 
+
+
+def adminka(request):
+    category=models.Category.objects.all()
+    product=models.Product.objects.all()
+
+    context={
+        'category':category,
+        'product':product
+    }
+
+    return render(request,'dashboard/index.html',context)
+
+
+def category_list(request):
+    categoriess=models.Category.objects.all()
+    return render(request,'dashboard/category/list.html',{'categoriess':categoriess})
+
+
+def category_create(request):
+    if request.method=='POST':
+        models.Category.objects.create(
+            name=request.POST['name']
+        )
+        return redirect('category_list')
+    return render(request,'dashboard/category/create.html')
+
+
+def category_update(request,id):
+    category=models.Category.objects.get(id=id)
+    if request.method=='POST':
+        category.name = request.POST['name']
+        category.save()
+        return redirect('category_list')
+    return render(request,'dashboard/category/update.html',{'category':category})
+
+
+
+def category_delete(request,id):
+    category=models.Category.objects.get(id=id)
+    category.delete()
+
+    return redirect('category_list')
+
+
+
+
+
+
+
+def product(request):
+    products=models.Product.objects.all()
+
+    return render(request,'dashboard/product/list.html',{'products':products})
+
+
+def product_create(request):
+    if request.method=='POST':
+        
+        name=request.POST['name'],
+        description=request.POST['description'],
+        quantity=request.POST['quantity'],
+        price=request.POST['price'],
+        currency=request.POST['currency'],
+        discount_price=request.POST['discount_price'],
+        baner_image= request.FILES['image'],
+        category = models.Category.objects.get(id=request.POST['category_id'])
+        models.Product.objects.create(
+            name=name,
+            description=description,
+            quantity=quantity,
+            price=price,
+            currency=currency,
+            discount_price=discount_price,
+            baner_image=baner_image,
+            category=category
+        )
+        return redirect('product')
+        
+    return render(request,'dashboard/product/create.html',{'category':models.Category.objects.all()})
+
+
+def product_update(request,id):
+    product=models.Product.objects.get(id=id)
+    if request.method=='POST':
+        product.name=request.POST['name']
+        product.description=request.POST['description']
+        product.quantity=request.POST['quantity']
+        product.currency=request.POST['currency']
+        product.discount_price=request.POST['discount_price']
+        product.baner_image=request.POST['baner_image']
+        product.category=request.POST['category']
+
+        product.save()
+
+        return redirect('product')
+    category=models.Category.objects.all()
+
+    context={
+        'product':product,
+        'category':category
+
+    }
+    
+    return render(request,'dashboard/product/update.html',context)
+
+
+
+
+
+
+
+def product_delete(request,id):
+    product=models.Product.objects.get(id=id)
+    product.delete()
+
+    return redirect('product')
